f76d1ae73fd2feab43bda71a21d3cf86bf1b69a5
Arianxx <34032486+Arianxx@users.noreply.github.com>
2018-09-07 21:47:18 +0800

    fix(discussion): improper notification conditions

--- Discussion/signal.py
+++ Discussion/signal.py
@@ -1,15 +1,27 @@
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 from Discussion.models import DiscussReply, Notification
 
 
-@receiver(post_save, sender=DiscussReply)
 def send_notification(sender, instance, created, **kwargs):
-    # todo: 回复自己不通知
     if created:
-        notify = Notification(sender=instance.user, instance=instance)
-        notify.save()
-        notify.receivers.set((instance.reply_to if len(instance.reply_to.all()) else []) + [instance.user])
+        if len(instance.reply_to):
+            notify = Notification(sender=instance.user, instance=instance)
+            notify.save()
+            receivers = set(instance.reply_to if len(instance.reply_to.all()) else [])
+            try:
+                receivers.remove(instance.user)
+            except KeyError:
+                pass
+            notify.receivers.set(receivers)
+
+        if instance.user != instance.discuss.user:
+            notify = Notification(sender=instance.user, instance=instance)
+            notify.save()
+            receivers = [instance.discuss.user]
+            notify.receivers.set(receivers)
 
     return True
+
+
+post_save.connect(send_notification, sender=DiscussReply)
