b758ea1fd4016dd7a00ccbde3cf87d717736465d
Arianxx <34032486+Arianxx@users.noreply.github.com>
2018-09-14 10:21:26 +0800

    fix(bg): make untransparent form to transparent

--- User/urls.py
+++ User/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.shortcuts import reverse
+from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
     PasswordResetCompleteView
 from django.urls import path
@@ -12,21 +12,21 @@ urlpatterns = [
     path('login', LoginView.as_view(template_name='Auth/login.html'), name='login'),
     path('logout', LogoutView.as_view(template_name='Auth/logout.html'), name='logout'),
     path('password-change', password_change, name='password_change'),
+    path('password-reset-done', PasswordResetDoneView.as_view(template_name='Auth/password_reset_done.html'),
+         name='password_reset_done'),
     path('password-reset', PasswordResetView.as_view(
         template_name='Auth/password_reset.html',
         email_template_name='Auth/email/password_reset_email.html',
         subject_template_name='Auth/email/password_reset_subject.txt',
-        success_url=reverse('User:password_reset_done'),
+        success_url=reverse_lazy('User:password_reset_done'),
         from_email=getattr(settings, 'EMAIL_FROM'),
     ), name='password_reset'),
-    path('password-reset-done', PasswordResetDoneView.as_view(template_name='Auth/password_reset_done.html'),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(
-            template_name='Auth/password_reset_confirm.html',
-            success_url=reverse('User:password_reset_complete'),
-    ), name='password_reset_confirm'),
     path('password-reset-complete', PasswordResetCompleteView.as_view(template_name='Auth/password_reset_complete.html'),
          name='password_reset_complete'),
+    path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(
+            template_name='Auth/password_reset_confirm.html',
+            success_url=reverse_lazy('User:password_reset_complete'),
+    ), name='password_reset_confirm'),
     path('register', register, name='register'),
     path('profile', profile, name='profile'),
     path('email-change', email_change, name='email_change'),
