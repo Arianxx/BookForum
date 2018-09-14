845c648a34859e42f261bd8f92b8e67d54d223e9
Arianxx <34032486+Arianxx@users.noreply.github.com>
2018-09-14 10:13:22 +0800

    fix(user): error redirect url

--- User/urls.py
+++ User/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.shortcuts import reverse
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
     PasswordResetCompleteView
 from django.urls import path
@@ -15,14 +16,14 @@ urlpatterns = [
         template_name='Auth/password_reset.html',
         email_template_name='Auth/email/password_reset_email.html',
         subject_template_name='Auth/email/password_reset_subject.txt',
-        success_url='User:password_reset_done',
+        success_url=reverse('User:password_reset_done'),
         from_email=getattr(settings, 'EMAIL_FROM'),
     ), name='password_reset'),
     path('password-reset-done', PasswordResetDoneView.as_view(template_name='Auth/password_reset_done.html'),
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(
             template_name='Auth/password_reset_confirm.html',
-            success_url='User:password_reset_complete',
+            success_url=reverse('User:password_reset_complete'),
     ), name='password_reset_confirm'),
     path('password-reset-complete', PasswordResetCompleteView.as_view(template_name='Auth/password_reset_complete.html'),
          name='password_reset_complete'),
